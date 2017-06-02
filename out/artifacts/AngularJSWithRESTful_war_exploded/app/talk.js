06cbbb725d5d2be90a5bf6fbee05d49eb0da641b
Wei Tang <donatshanghai@gmail.com>
2017-06-02 12:08:45 -0400

    save before screw up

--- out/artifacts/AngularJSWithRESTful_war_exploded/app/talk.js
+++ out/artifacts/AngularJSWithRESTful_war_exploded/app/talk.js
@@ -1,5 +1,18 @@
-var app = angular.module("app",[ ]);
-app.controller("talkCntrl",function($scope,$http){
+var app = angular.module("app",['xeditable','ngRoute']);
+app.config(function($routeProvider,$locationProvider){
+    $locationProvider.hashPrefix = '';
+    $routeProvider
+        .when("/Talk",{
+            templateUrl: "talk.html"
+        })
+        .when('/AddTalk',{
+            template:'<h2>addtalk</h2>'
+        });
+});
+// app.run(function(editableOptions,$http) {
+//     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+// });
+app.controller("talkCntrl",function($scope,$http,$filter){
     $http.get("http://localhost:8080/RESTful/rest/talk/GetTalks").then(function(
        data
     ){
@@ -11,4 +24,43 @@ app.controller("talkCntrl",function($scope,$http){
             console.log(object.duration);
             $scope.count += object.duration*1;
         })});
+
+        $scope.removeTalk = function(index) {
+          $scope.talks.splice(index, 1);
+        };
+
+        $scope.addTalk = function(){
+            console.log("i am in addTalk");
+            $scope.inserted = {
+                duration: null,
+                id:null,
+                name:"",
+                speaker:"",
+                venue:""
+
+            };
+            $scope.talks.push($scope.inserted);
+        };
+
+        $scope.saveTalk = function(data){
+            console.log(data);
+            $http(
+                {
+                    method:'POST',
+                    url:'http://localhost:8080/RESTful/rest/talk/Update',
+                    params:{
+                        id:data.id,
+                        name:data.name,
+                        speaker:data.speaker,
+                        venue:data.venue,
+                        duration:data.duration
+                    }
+                }
+
+            ).then(function(data){
+                alert("ok");
+
+            });
+            console.log($http);
+        };
 });
