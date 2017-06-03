ffd908d5764222e995e45c7e9b301c5cba71ad76
Wei Tang <donatshanghai@gmail.com>
2017-06-02 22:45:37 -0400

    fix ng-repeat on single item
    fix ng-repeat on single item

--- web/app/talk.js
+++ web/app/talk.js
@@ -21,60 +21,61 @@ app.directive("myDir",function(){
 //     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 // });
 app.controller("talkCntrl",function($scope,$http,$filter){
-    $http.get("http://localhost:8080/RESTful/rest/talk/GetTalks").then(function(
-       data
-    ){
+
+    $http.get("http://localhost:8080/RESTful/rest/talk/GetTalks").then(function(data){
+        console.log("I am here in talkcontrol http get");
+        console.log(data);
         console.log(data.data);
+        console.log(data.data.talk);
+        if (angular.isArray(data.data.talk))
         $scope.talks = data.data.talk;
+        else{
+            $scope.talks = [data.data.talk];
+        }
         $scope.count = 0;
 
         angular.forEach(data.data.talk, function(object) {
-            console.log(object.duration);
             $scope.count += object.duration*1;
         })});
 
-        $scope.removeTalk = function(index) {
-          $scope.talks.splice(index, 1);
+    $scope.removeTalk = function(index) {
+        $scope.talks.splice(index, 1);
+    };
+
+    $scope.addTalk = function(){
+        $scope.inserted = {
+            duration: null,
+            id:null,
+            name:"",
+            speaker:"",
+            venue:""
+
         };
+        $scope.talks.push($scope.inserted);
+    };
 
-        $scope.addTalk = function(){
-            console.log("i am in addTalk");
-            $scope.inserted = {
-                duration: null,
-                id:null,
-                name:"",
-                speaker:"",
-                venue:""
+    $scope.saveTalk = function(data){
+        return $http(
+            {
+                method:'POST',
+                url:'http://localhost:8080/RESTful/rest/talk/Update',
+                headers:{'Content-Type': 'application/x-www-form-urlencoded'},
+                transformRequest: function(obj) {
+                    var str = [];
+                    for(var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                },
+                data:data
+            })
+    };
+    $scope.deleteTalk = function(id,index){
+        $http.delete("http://localhost:8080/RESTful/rest/talk/"+id).then(function(){
+            $scope.talks.splice(index, 1);}
+        );
+    };
+    $scope.submit = function(form){
 
-            };
-            $scope.talks.push($scope.inserted);
-        };
-
-        $scope.saveTalk = function(data){
-            console.log(data);
-            return $http(
-                {
-                    method:'POST',
-                    url:'http://localhost:8080/RESTful/rest/talk/Update',
-                    headers:{'Content-Type': 'application/x-www-form-urlencoded'},
-                    transformRequest: function(obj) {
-                        var str = [];
-                        for(var p in obj)
-                            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                        return str.join("&");
-                    },
-                    data:data
-                })
-        };
-        $scope.deleteTalk = function(id){
-            console.log(id);
-
-            $http.delete("http://localhost:8080/RESTful/rest/talk/"+id).then(function(){
-                $scope.talks.splice(index, 1);}
-            );
-        };
-        $scope.submit = function(form){
-
-        }
+    }
 
 });
